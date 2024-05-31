267fe0a413b85055756a23dfc898d011956092e0
j.salas2016 <jsalas198016@gmail.com>
2024-05-31 14:36:36 -0400

    almost done i have done most of the instructions

--- app.js
+++ app.js
@@ -16,13 +16,6 @@ secTeo.style.color = 'var(--seventh-color)';
 secTeo.style.fontSize = '1.5em';
 secTeo.style.textShadow = '3px 3px 6px black';
 
-
-const secCare = document.querySelector('.fa-house');
-secCare.style.color = 'green';
- 
-const homeSec = document.querySelector("#homeColor");
-homeSec.style.color = "red";
-
 const navNav = document.querySelectorAll('section');
 navNav.forEach(navNav =>{
     navNav.style.background = 'var(--darkBlue)'
@@ -33,11 +26,18 @@ navNav.forEach(navNav =>{
 
 })
 
+    const h2 = document.querySelector('h1');
+        h2.style.color = 'darkblue';
+        h2.style.textShadow = "4px 4px 6px white";
+        h2.style.paddingLeft = '110px';
+
+
 const newEl  = document.getElementById('newDiv')
 const sideNav = document.createElement('p')
-newEl.textContent = "Hi and Welcome this our:  services!";
+newEl.textContent = "Hi and Welcome this our page!";
 newEl.style.position = 'relative';
-newEl.style.top = '500px'
+newEl.style.zIndex = '10'
+newEl.style.top = '-400px'
 newEl.style.right = '47em';
 newEl.style.bottom = '-200px';
 newEl.style.borderRadius = "30%"
@@ -49,19 +49,10 @@ newEl.style.background = 'var(--primary-color)';
 newEl.style.boxShadow = '2px 2px 2px black';
 newEl.appendChild(sideNav);
 
+const addListToNewDev = document.getElementById('newDev')
+const newli = document.createDocumentFragment();
 
-let text = document.getElementById('newDiv');
 
-
-text.addEventListener('click', () =>{
-text.style.background = 'lightblue';
-        if(text.textContent == "services!"){
-            text.textContent = " Contact!"
-        }  else{
-            text.textContent = " form!"
-        }
-
-})
 
 const footer = document.body
     footer.addEventListener('click', () =>{
@@ -85,22 +76,73 @@ const footer = document.body
         foot.style.background = 'var(--darkBlue)';
    })
 
-   const list = document.querySelector('a');
+   const list = document.querySelector('li a');
    list.addEventListener('mouseenter', () =>{
-            list.style.background = 'white';
             list.style.fontSize = '1em';
-            list.style.color = 'red';
-            list.style.textShadow = 'none'
+            list.style.color = 'white';
+            list.style.borderRadius = '50%';
+            list.style.border = '2px solid white';
+
+
 
             list.addEventListener('mouseleave', ()=>{
-                list.style.background = 'var(--darkBlue)';
                 list.style.color = 'white';
                 list.style.textShadow = '2px 2px 3px black'
                 list.style.borderRadius = '50%';
-                list.style.border = '2px solid var(--h1-color)'
+                list.style.border = '2px solid var(--darkBlue)'
             })
    })
 
- 
+ const icon = document.querySelector('.fa-house');
+ icon.style.color = 'white';
+ icon.style.textShadow = '2px 2px 3px black'
 
 
+const nav = document.querySelector('nav')
+nav.style.background = 'var(--darkBlue)'
+nav.style.height = '139px';
+
+const navA = document.querySelectorAll('nav a')
+navA.forEach(item =>{
+    item.style.color = 'white';
+    item.style.textShadow = '2px 2px 3px blue';
+    item.addEventListener('mouseover', ()=>{
+        item.style.color = 'red'
+        item.style.textShadow = 'none';
+        
+        item.addEventListener('mouseleave', ()=>{
+            item.style.color = 'white'
+            item.style.textShadow = '2px 2px 1px red';
+            item.style.fontSize = '1.1em';
+
+
+        })
+
+    })
+})
+
+
+
+    const message = document.getElementById('quickMessage');
+    const quicky = document.createDocumentFragment();
+
+    let qMessage = document.createElement('li');
+
+    qMessage.textContent = 'Please fill out the form above for a quote about certain services'
+    
+    quicky.appendChild(qMessage);
+    message.appendChild(quicky);
+
+    const changeStyle = document.getElementById('quickMessage');
+    changeStyle.style.color = 'red';
+    changeStyle.style.listStyle = 'none'
+    changeStyle.style.fontWeight = 'bold';
+
+  function Validation(){
+    let errorOne = document.forms['quoteForm']['question-section'].value;
+
+    if(errorOne == ""){
+        alert('What services do you need!!')
+        return false;
+    }
+  }
